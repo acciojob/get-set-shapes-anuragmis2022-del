eafee6eaedf689c7e1af336731326f3c7ac18334
anuragmis2022-del <anuragmis2022@gmail.com>
2026-06-11 13:46:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,66 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+	constructor(width, height)
+	{
+		this.width=w
+		this.height=h
+	}
+	get width()
+	{
+		return this.width
+	}
+	het height()
+	{
+		return this.height
+	}
+getArea()
+	{
+		let area=this.width*this.height
+		return area 
+	}
+}
 
-class Square extends Animal {}
+class Square extends Rectangle {
+	constructor(width, height, side )
+	{
+		super(width, height)
+		this.side=side
+	}
+	getperimeter()
+	{
+		let peri= 4*this.side
+	}
+		
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
 window.Square = Square;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
